f8f96d64e54dbbec42257c2fb2ba9b3ca36d68c4
FasihLisan <fasihullisan091966@gmail.com>
2024-03-21 03:22:31 +0700

    integration ci circle-ci

--- core/proguard-rules.pro
+++ core/proguard-rules.pro
@@ -20,3 +20,6 @@
 # hide the original source file name.
 #-renamesourcefileattribute SourceFilez
 
+# Please add these rules to your existing keep rules in order to suppress warnings.
+# This is generated automatically by the Android Gradle plugin.
+-dontwarn java.lang.invoke.StringConcatFactory
--- core/src/main/java/com/example/core/data/local/room/UserRoomDatabase.kt
+++ core/src/main/java/com/example/core/data/local/room/UserRoomDatabase.kt
@@ -4,7 +4,7 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.example.core.domain.model.FavoriteUser
 
-@Database(entities = [FavoriteUser::class], version = 1)
+@Database(entities = [FavoriteUser::class], version = 1, exportSchema = false)
 abstract class UserRoomDatabase: RoomDatabase() {
     abstract fun userDao(): UserDao
 }
